5030d06404885f3dd37e512831c3a4bca0bd9626
Sruthi17122001 <63628466+Sruthi17122001@users.noreply.github.com>
2020-05-22 14:27:53 +0530

    Created list.py
    this will output the positive numbers only

---
+++ list.py
@@ -0,0 +1,11 @@
+list1=[12,-7,5,64,-14]
+for i in list1:
+    if i>0:
+        print(i, end=" ")
+        
+list2=[12,14,-95,3]
+lst2=[]
+for i in list2:
+    if i>0:
+        lst2.append(i)
+print(lst2)
